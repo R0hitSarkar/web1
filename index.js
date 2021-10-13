1101a7112e83bb748abff1081f42085287f778e8
R0hitSarkar <92440248+R0hitSarkar@users.noreply.github.com>
2021-10-13 18:16:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,307 @@
+console.log("Included");
+b1 = document.getElementById("b2006");
+b2 = document.getElementById("b2007");
+b3 = document.getElementById("b2008");
+b4 = document.getElementById("b2009");
+b5 = document.getElementById("b2010");
+b6 = document.getElementById("b2011");
+b7 = document.getElementById("b2012");
+b8 = document.getElementById("b2013");
+b9 = document.getElementById("b2014");
+b10 = document.getElementById("b2015");
+b11 = document.getElementById("b2016");
+b12 = document.getElementById("b2017");
+b13 = document.getElementById("b2018");
+b14 = document.getElementById("b2019");
+b15 = document.getElementById("b2020");
+t1 = document.getElementById("true1");
+f1 = document.getElementById("false1");
+t2 = document.getElementById("true2");
+f2 = document.getElementById("false2");
+b1.addEventListener('click',function(e){
+    let v = /2006/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b2.addEventListener('click',function(e){
+    let v = /2007/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b3.addEventListener('click',function(e){
+    let v = /2008/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b4.addEventListener('click',function(e){
+    let v = /2009/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b5.addEventListener('click',function(e){
+    let v = /2010/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b6.addEventListener('click',function(e){
+    let v = /2011/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b7.addEventListener('click',function(e){
+    let v = /2012/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b8.addEventListener('click',function(e){
+    let v = /2013/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b9.addEventListener('click',function(e){
+    let v = /2014/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b10.addEventListener('click',function(e){
+    let v = /2015/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b11.addEventListener('click',function(e){
+    let v = /2016/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b12.addEventListener('click',function(e){
+    let v = /2017/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b13.addEventListener('click',function(e){
+    let v = /2018/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b14.addEventListener('click',function(e){
+    let v = /2019/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+b15.addEventListener('click',function(e){
+    let v = /2020/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+t1.addEventListener('click',function(e){
+    let v = /t1/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+f1.addEventListener('click',function(e){
+    let v = /f1/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+
+t2.addEventListener('click',function(e){
+    let v = /t2/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+f2.addEventListener('click',function(e){
+    let v = /f2/;
+    let r = document.getElementsByClassName("only");
+    for(let j=0;j<r.length;j++){
+        r[j].style.display = "block";
+    		let x = r[j].getAttribute('class');
+        console.log(j);
+        console.log("x= ",x);
+        console.log("v= ",v.source);
+        if(v.exec(x)===null){
+        	r[j].style.display="none";
+        }
+    }
+
+});
+
